999846888596606563b3ad85c2af88f97bda5469
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-22 14:21:44 +0100

    set ui action creators

--- src/redux/actions/ui.js
+++ src/redux/actions/ui.js
@@ -1,2 +1,9 @@
 // action_types;
 const SET_LOADER = 'SET_LOADER';
+
+// action creators
+export const setLoader = ({ state, feature }) => ({
+  type: `${feature} ${SET_NOTIFICATION}`,
+  payload: state,
+  meta: { feature },
+});
