74268a1691d5653f2738490bd3bed3d03455d069
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-22 14:19:42 +0100

    set api action creators

--- src/redux/actions/api.js
+++ src/redux/actions/api.js
@@ -2,3 +2,22 @@
 const API_SUCCESS = 'API_SUCCESS';
 const API_ERROR = 'API_ERROR';
 const API_REQUEST = 'API_REQUEST';
+
+//action creators
+export const apiRequest = ({ body, method, url, feature }) => ({
+  type: `${feature} ${API_REQUEST}`,
+  payload: body,
+  meta: { method, url, feature },
+});
+
+export const apiSuccess = ({ response, feature }) => ({
+  type: `${feature} ${API_REQUEST}`,
+  payload: response,
+  meta: { feature },
+});
+
+export const apiError = ({ error, feature }) => ({
+  type: `${feature} ${API_ERROR}`,
+  payload: error,
+  meta: { feature },
+});
