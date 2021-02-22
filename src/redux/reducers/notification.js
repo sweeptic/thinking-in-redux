92d1d28cdcedbe1b6d70f3aaa1270fc5372acd08
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-22 14:32:57 +0100

    notification reducer

--- src/redux/reducers/notification.js
+++ src/redux/reducers/notification.js
@@ -1,1 +1,12 @@
 import { SET_NOTIFICATION } from '../actions/notification';
+
+const initState = [];
+
+export const notificationsReducer = (notifications = initState, action) => {
+  switch (true) {
+    case action.type.includes(SET_NOTIFICATION):
+      return [...notifications, action.payload];
+    default:
+      return notifications;
+  }
+};
