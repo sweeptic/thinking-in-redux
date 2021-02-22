25aa9418405c9e9b6b168cab59badd8c54723f2c
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-22 14:02:17 +0100

    delete actions

--- src/redux/actions/data.js
+++
@@ -1,6 +0,0 @@
-const DATA_NORMALIZED = 'DATA_NORMALIZED';
-
-export const dataNormalized = ({feature}) => ({
-  type: `${feature} ${DATA_NORMALIZED}`,
-  meta: {feature}
-});
--- src/redux/actions/ui.js
+++
@@ -1,9 +0,0 @@
-// action types
-export const SET_LOADER = 'SET_LOADER';
-
-// action creators
-export const setLoader = ({state, feature}) => ({
-  type: `${feature} ${SET_LOADER}`,
-  payload: state,
-  meta: {feature}
-});
