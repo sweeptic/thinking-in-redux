5d51c73eb94d7123c218a4f8a3772a42f3f4dbec
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-22 14:20:53 +0100

    set notification action creators

--- src/redux/actions/notification.js
+++ src/redux/actions/notification.js
@@ -1,2 +1,9 @@
 // action_types;
 const SET_NOTIFICATION = 'SET_NOTIFICATION';
+
+// action creators
+export const setNotification = ({ message, feature }) => ({
+  type: `${feature} ${SET_NOTIFICATION}`,
+  payload: message,
+  meta: { feature },
+});
