d6647be3eb84e10a472ca34ad142d0e3d092f4b4
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-22 14:38:30 +0100

    ui reducer

--- src/redux/reducers/ui.js
+++ src/redux/reducers/ui.js
@@ -1,1 +1,14 @@
 import { SET_LOADER } from '../actions/ui';
+
+const initState = {
+  loading: false,
+};
+
+export const uiReducer = (ui = initState, action) => {
+  switch (true) {
+    case action.type.includes(SET_LOADER):
+      return { ...ui, loading: action.payload };
+    default:
+      return ui;
+  }
+};
