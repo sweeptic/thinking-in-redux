1bc476623e2db3e9743d73f491560359ea26f3a6
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-22 18:32:36 +0100

    update npm

--- src/ui/Dispatchers.js
+++ src/ui/Dispatchers.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchBooks } from "../redux/actions/books"
+import { fetchBooks } from '../redux/actions/books';
 
-const Dispatchers = (props) => (
-  <div className="dispatchers">
-    <button className="dispatch-button" onClick={() => props.fetchBooks('')}><i className="fa fa-rocket" /></button>
-    <button className="dispatch-button"><i className="fa fa-undo" /></button>
+const Dispatchers = props => (
+  <div className='dispatchers'>
+    <button className='dispatch-button' onClick={() => props.fetchBooks('')}>
+      <i className='fa fa-rocket' />
+    </button>
+    <button className='dispatch-button'>
+      <i className='fa fa-undo' />
+    </button>
   </div>
 );
 
-export default connect(null,{ fetchBooks })(Dispatchers);
+export default connect(null, { fetchBooks })(Dispatchers);
