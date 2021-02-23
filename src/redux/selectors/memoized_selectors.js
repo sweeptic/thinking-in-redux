0a89673d8dbe8c856e2ed9ba2d85c8d445f6d427
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-23 13:57:12 +0100

    add selectors text

--- src/redux/selectors/memoized_selectors.js
+++
@@ -1,5 +0,0 @@
-// A memoized selector caches its result and returns this cached result every time the selector is invoked
-// with the same arguments. This performance optimization technique is very efficient for selectors
-// that perform heavy calculations or traverse a large data set.
-// I encourage you to use the official selector library for Redux, reselect, which implements memoized
-// selectors while exposing a clean API for creating and composing selectors.
