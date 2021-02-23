d25f81f9363f4402bdd89db485bf912080a58c28
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-02-23 13:13:43 +0100

    add/remove comment

--- src/redux/reducers/stateFreezer.js
+++ src/redux/reducers/stateFreezer.js
@@ -3,6 +3,7 @@ import { deepFreeze } from '../../utils/deepFreeze';
 export function stateFreezer(reducer) {
   return function freezer(state, action) {
     // freeze the state and run the original reducer
+
     deepFreeze(state);
     const newState = reducer(state, action);
 
